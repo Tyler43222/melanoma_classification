af16f6d399d7eaa9ea678a15c96ae6614539368b
Halsey <halsey.tyler@icloud.com>
2025-12-31 11:05:48 -0500

    Update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from predict import analyze_image, guided_gradcam_png
 
 st.title("Skin Scan")
 st.write("")
-st.write("For accurate results make sure skinspot is: (1) large in frame, (2) in-focus, (3) brightly lit. Best if taken from a high-resolution digital camera")
+st.markdown("For accurate results make sure skinspot is: (1) large in frame, (2) in-focus, (3) brightly lit.<br>Best if taken from a high-resolution digital camera", unsafe_allow_html=True)
 st.write("")
 uploaded_file = st.file_uploader("Upload a file", type=["png", "jpg", "jpeg"])
 st.write("")
@@ -16,9 +16,9 @@ if st.button("Analyze"):
             image_bytes = uploaded_file.getvalue()
             result = analyze_image(io.BytesIO(image_bytes))
             st.write("")
-            st.write("These results are not a diagnosis")
+            st.write(" *These results are not a diagnosis*")
             st.write("")
-            st.markdown(f"RESULTS:\n<pre>      Benign: {result[0]}\n      Malignant: {result[1]}</pre>", unsafe_allow_html=True)
+            st.markdown(f"RESULTS:  Benign: {result[0]}%,  Malignant: {result[1]}%", unsafe_allow_html=True)
             st.write("")
 
             # Generate the Guided Grad-CAM image
